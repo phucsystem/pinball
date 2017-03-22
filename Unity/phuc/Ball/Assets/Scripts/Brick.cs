bf4d2d86c5d41b0b1ca85c88996b0b6be2c6d66e
phuc dang <phucsystem@gmail.com>
2017-03-22 08:48:48 +0700

    Add demo for Ball movememnt

---
+++ Unity/phuc/Ball/Assets/Scripts/Brick.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Brick : MonoBehaviour {
+
+	public Sprite[] hitSprites;
+
+	private int timesHit;
+	private LevelManager levelManager;
+	
+	// Use this for initialization
+	void Start () {
+		timesHit = 0;
+		levelManager = GameObject.FindObjectOfType<LevelManager>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+	
+	void OnCollisionEnter2D (Collision2D col) {
+		timesHit++;
+		int maxHits = hitSprites.Length + 1;
+		if (timesHit >= maxHits) {
+			Destroy(gameObject);
+		} else {
+			LoadSprites();
+		}
+	}
+	
+	void LoadSprites () {
+		int spriteIndex = timesHit - 1;
+		if (hitSprites[spriteIndex]) {
+			this.GetComponent<SpriteRenderer>().sprite = hitSprites[spriteIndex];
+		}
+	}
+	
+	// TODO Remove this method once we can actually win!
+	void SimulateWin () {
+		levelManager.LoadNextLevel();
+	}
+}
