8440b7971e973b771c31cfd571a313ee63badde5
phuc dang <phucsystem@gmail.com>
2017-03-22 10:45:17 +0700

    Update ball demo

---
+++ Unity/phuc/Ball/Assets/Scripts/ButtonManager.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ButtonManager : MonoBehaviour
+{
+
+	private Paddle paddle;
+
+	void Start ()
+	{
+		paddle = FindObjectOfType<Paddle> ();
+	}
+
+	void Update(){
+		if (Input.GetKeyDown (KeyCode.LeftArrow)) {
+			movePaddle (-1);
+		}
+
+		if (Input.GetKeyDown (KeyCode.RightArrow)) {
+			movePaddle (+1);
+		}
+	}
+
+	public void clickLeft ()
+	{
+		movePaddle (-1);
+
+	}
+
+	public void clickRight ()
+	{
+		movePaddle (1);
+	}
+
+	private void movePaddle (int moveDistance)
+	{			
+		Vector3 paddlePos = paddle.transform.position;
+		paddlePos.x += moveDistance;
+		paddle.transform.position = paddlePos;
+	}
+}
--- Unity/phuc/Ball/Assets/Scripts/Paddle.cs
+++ Unity/phuc/Ball/Assets/Scripts/Paddle.cs
@@ -10,12 +10,12 @@ public class Paddle : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		Vector3 paddlePos = new Vector3 (0.5f, this.transform.position.y, 0f);
-	
-		float mousePosInBlocks = Input.mousePosition.x / Screen.width * 16;
-		
-		paddlePos.x = Mathf.Clamp(mousePosInBlocks, 0.5f, 15.5f);
-		
-		this.transform.position = paddlePos;
+//		Vector3 paddlePos = new Vector3 (0.5f, this.transform.position.y, 0f);
+//	
+//		float mousePosInBlocks = Input.mousePosition.x / Screen.width * 16;
+//		
+//		paddlePos.x = Mathf.Clamp(mousePosInBlocks, 4.5f, 11.5f);
+//		
+//		this.transform.position = paddlePos;
 	}
 }
